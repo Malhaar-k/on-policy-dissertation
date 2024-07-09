f65c3a4580ea23a80d3ffd0b41ba16f57d54ca12
Malhaar-k <malhaar.karandikar@gmail.com>
2024-07-09 13:05:50 +0100

    Docker image experiments

--- install_sc.sh
+++ install_sc.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-unzip /scratch/SC2.4.10.zip -P iagreetotheeula -d /scratch/SCFiles/StarCraftII
-rm -rf /scratch/SC2.4.10.zip
 
-export SC2PATH="/scratch/SCFiles/StarCraftII"
+export SC2PATH="$PWD/SCFiles/StarCraftII"
 export MAP_DIR="$SC2PATH/Maps"
+
+if [ ! -d $SC2PATH ]; then
+        mkdir -p $SC2PATH
+fi
+
+unzip $PWD/SC2.4.10.zip -P iagreetotheeula -d $SC2PATH
+
+
 
 if [ ! -d $MAP_DIR ]; then
         mkdir -p $MAP_DIR
 fi
 
-unzip SMAC_Maps.zip
+unzip SMAC_Maps.zip -d $MAP_DIR
 mkdir "$MAP_DIR/SMAC_Maps"
 mv *.SC2Map "$MAP_DIR/SMAC_Maps"
+rm -rf /scratch/SC2.4.10.zip
+
 echo 'StarCraft II and SMAC are installed.'
